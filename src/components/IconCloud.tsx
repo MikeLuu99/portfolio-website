393707ba1f01b8c6a8619c51131a7978d9112eab
miklew <trungducmic@gmail.com>
2024-08-15 09:10:12 -0700

    update: modified IconCloud

--- src/components/IconCloud.tsx
+++ src/components/IconCloud.tsx
@@ -1,41 +1,41 @@
 import IconCloud from "@/components/magicui/icon-cloud";
 
-const slugs = [
-  "typescript",
-  "javascript",
-  "dart",
-  "java",
-  "react",
-  "flutter",
-  "android",
-  "html5",
-  "css3",
-  "nodedotjs",
-  "express",
-  "nextdotjs",
-  "prisma",
-  "amazonaws",
-  "postgresql",
-  "firebase",
-  "nginx",
-  "vercel",
-  "testinglibrary",
-  "jest",
-  "cypress",
-  "docker",
-  "git",
-  "jira",
-  "github",
-  "gitlab",
-  "visualstudiocode",
-  "androidstudio",
-  "sonarqube",
-  "figma",
-];
+// const slugs = [
+//   "typescript",
+//   "javascript",
+//   "dart",
+//   "java",
+//   "react",
+//   "flutter",
+//   "android",
+//   "html5",
+//   "css3",
+//   "nodedotjs",
+//   "express",
+//   "nextdotjs",
+//   "prisma",
+//   "amazonaws",
+//   "postgresql",
+//   "firebase",
+//   "nginx",
+//   "vercel",
+//   "testinglibrary",
+//   "jest",
+//   "cypress",
+//   "docker",
+//   "git",
+//   "jira",
+//   "github",
+//   "gitlab",
+//   "visualstudiocode",
+//   "androidstudio",
+//   "sonarqube",
+//   "figma",
+// ];
 
 export default function IconCloudDemo({ skills }: { skills: string[] }) {
   return (
-    <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg bg-background px-20 pb-20 pt-8 ">
+    <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg bg-background mb-20">
       <IconCloud iconSlugs={skills} />
     </div>
   );
